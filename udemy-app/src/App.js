d7a98e1f452f5bef4792035d45e12b4d57e6c3e5
tansen <tansenkhan1990@gmail.com>
2021-03-13 10:37:37 +0100

    best practice

--- udemy-app/src/App.js
+++ udemy-app/src/App.js
@@ -4,16 +4,16 @@ import './App.css';
 
 class App extends Component {
   render() {
-   // return (
-      // <div className="App">
-      //   <h1>
-      //   Hi it's my first app
-      //   </h1>
-      // </div>
-    // );
-  // }
-  return React.createElement('div',null,React.createElement('h1',{className:"App"},'this is for nested elements'));
+   return (
+      <div className="App">
+        <h1>
+        Hi it's my first app
+        </h1>
+      </div>
+    );
   }
+ // return React.createElement('div',null,React.createElement('h1',{className:"App"},'this is for nested elements'));
+  //}
 }
 
 export default App;
