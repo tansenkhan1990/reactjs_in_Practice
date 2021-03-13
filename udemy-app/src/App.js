4e603c5057f2bdfe63582912101c6fbb539a4ae1
tansen <tansenkhan1990@gmail.com>
2021-03-13 10:51:24 +0100

    import Person and add element from Person

--- udemy-app/src/App.js
+++ udemy-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NewPerson from './Person/Person.js'
 
 class App extends Component {
   render() {
@@ -9,11 +10,12 @@ class App extends Component {
         <h1>
         Hi it's my first app
         </h1>
+        <NewPerson></NewPerson>
       </div>
     );
   }
  // return React.createElement('div',null,React.createElement('h1',{className:"App"},'this is for nested elements'));
-  //}
-}
+  }
+
 
 export default App;
---
+++ udemy-app/src/Person/Person.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const person = () => {
+    return <p> hello i am from Person component  </p>
+}
+export default person;
