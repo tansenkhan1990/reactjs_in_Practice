db987535deedb019230d643ada168fef17241b68
tansen <tansenkhan1990@gmail.com>
2021-03-15 16:04:44 +0100

    adding dynamic classes

--- udemy-app/src/App.js
+++ udemy-app/src/App.js
@@ -84,7 +84,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p className= {classes} > This is really working!</p>
+        <p className= {classes.join(' ')} > This is really working!</p>
         <button
           style = { style }
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
